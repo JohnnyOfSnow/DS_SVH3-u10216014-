b867e3e2c3b1d52f7a0035687c84d4760a1ef1ea
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-08-20 23:53:21 +0800

    加入自由選題的功能
    我採用一個名為 capacity.txt 的文件先載入題目有多少題
    然後每題之間用  -   來分開，若是題目太長我先用 StringBuilder 儲存，readLine()一次讀一行，使用方法append連結
    ，當遇到 - 時就是要換下一題，因此組合好的StringBuilder轉成 Sring 再丟入字串陣列。
    最後當題目全數放置好時，利用Math.random() 隨機亂數一個整數，對應到的題目顯示出來。

--- ReadFileTest.java
+++ ReadFileTest.java
@@ -1,22 +1,58 @@
 import java.io.*;
+import java.util.Scanner;
+
 public class ReadFileTest {
-	public static void main(String[] args) {
-		FileReader fr = null;
-		BufferedReader br = null;
-		try {
-			fr = new FileReader("question.txt");
-			br = new BufferedReader(fr);
-			String data;
-			while ((data = br.readLine()) != null) {
-				System.out.println(data);
+	public static void main(String[] args) throws Exception{
+		java.io.File file = new java.io.File("capacity.txt"); // A file use to tell the program how many question do we have.
+
+		if(file.exists()) {
+			Scanner input = new Scanner(file);
+			String capacityForString = " ";
+			while (input.hasNext()) {
+				capacityForString = input.next(); // Read items
 			}
-		}
-		catch(IOException e){}
-		finally{
+
+			input.close(); // Close the file.
+			int capacityForInt = Integer.parseInt(capacityForString); // String to integer.
+			capacityForInt = capacityForInt + 1; // Avoid the index 0, so add one.
+			FileReader fr = null;
+			BufferedReader br = null;
+			String[] question = new String[capacityForInt];
 			try {
-				br.close();
-			}
+				fr = new FileReader("question.txt"); // A file holds the question.
+				br = new BufferedReader(fr);
+				String data;
+				StringBuilder stringBuilderForQuestion = new StringBuilder();
+				int index = 1;
+				
+				while ((data = br.readLine()) != null) {
+					if (data.equals("-")) {
+						String stringForQuestion = new String(stringBuilderForQuestion); // Convert stringBuilder into String data type
+						question[index] = stringForQuestion; // A question has connected, so we input the string array.
+						index = index + 1;
+						stringBuilderForQuestion.delete(0, stringForQuestion.length()); // clear the StringBuilder.
+					} else {	
+						stringBuilderForQuestion.append(data + "\n"); // connect the long question.
+					}
+				
+				}
+			} // end try
 			catch(IOException e){}
-		}		
-	}
-}
+			finally{
+				try {
+					br.close(); // Because the br = new BufferedReader(fr); , we close the br.
+				}
+				catch(IOException e){}
+			} // end finally
+
+			int chooseQuestion = (int)(Math.random() * capacityForInt); // choose a question for random.
+			while(chooseQuestion == 0) {
+				chooseQuestion = (int)(Math.random() * capacityForInt);
+			}
+				System.out.println(question[chooseQuestion]); // display the question on CMD.
+			
+		} else {
+			System.out.println("You loss a file which decide the capacity,so we cannot load the question."); // Wrong message for check.
+		} // end if		
+	}// end main method
+}// end class ReadFileTest
