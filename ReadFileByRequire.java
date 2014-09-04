10b8bb99568c121dee4c541c6447762d0594253d
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-09-04 11:02:17 +0800

    增加得到答案的程式碼
    加入2個方法
    1.從answer.txt得到題目答案
    2.將答案做第1次分割

--- ReadFileByRequire.java
+++ ReadFileByRequire.java
@@ -13,12 +13,19 @@ public class ReadFileByRequire{
 
 			input.close(); // Close the file.
 			int capacityForInt = Integer.parseInt(capacityForString); // String to integer.
-			int chooseQuestion = (int)(Math.random() * capacityForInt); // choose a question for random.
+			int chooseQuestion = (int)(Math.random() * capacityForInt) + 1; // choose a question for random.
 			while(chooseQuestion == 0) {
 				chooseQuestion = (int)(Math.random() * capacityForInt);
 			}
+			System.out.println("Ramdom result is " + chooseQuestion);
 			handleQuestion test1 = new handleQuestion();
 			System.out.print(test1.giveQuestion(chooseQuestion));
+			String answer = test1.getAnswer(chooseQuestion);
+			String[] alreadyAnswer = test1.handleAnswer(answer);
+
+			for(int i = 0; i < alreadyAnswer.length; i++){
+				System.out.println(alreadyAnswer[i]);
+			}
 		} else {
 			System.out.println("Sorry, we can't find the file that decide how many question.");
 		}
@@ -33,7 +40,6 @@ class handleQuestion{
 	}
 
 	public static String giveQuestion(int number){
-		java.io.File file = new java.io.File("capacity.txt"); // A file use to tell the program how many question do we have.
 
 		String question = ""; 
 		
@@ -74,4 +80,49 @@ class handleQuestion{
 		return question;
 				
 	}// end method giveQuestion
+
+	static String getAnswer(int number){
+		String answer = ""; 
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader("answer.txt"); // A file holds the question.
+			br = new BufferedReader(fr);
+			String data;
+			StringBuilder stringBuilderForAnswer = new StringBuilder();
+			int index = 1;
+			int times = 1;
+				
+			while (((data = br.readLine()) != null) && (times <= number)) {
+				if (data.equals("-")) {
+					String stringForAnswer = new String(stringBuilderForAnswer); // Convert stringBuilder into String data type
+					if (times == number) {
+						answer = stringForAnswer;
+						break;
+					}
+					stringBuilderForAnswer.delete(0, stringForAnswer.length()); // clear the StringBuilder.
+					times = times + 1;
+				} else {	
+					stringBuilderForAnswer.append(data + "\n"); // connect the long answer.
+				}
+				
+			}
+		} // end try
+		catch(IOException e){}
+		finally{
+			try {
+				br.close(); // Because the br = new BufferedReader(fr); , we close the br.
+			}
+			catch(IOException e){}
+		} // end finally
+
+		return answer;
+	}// end method getAnswer
+
+	static String[] handleAnswer(String needSeparate){
+		String[] firstHandleAnswer = needSeparate.split(",");
+		return firstHandleAnswer;
+	}// end method handleAnswer
 }// end class handleQuestion
