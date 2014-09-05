43583003643cf32fca4cfba30c006b8ce51f3794
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-09-05 11:56:28 +0800

    加入方法correspondAnswer
    correspondAnswer 處理對答案的動作
    
    今天遇到了一個問題，就是最後1個答案貼正確答案也錯，原因在於方法getAnswer裡的這行
    stringBuilderForAnswer.append(data + "\n"); 會使答案字串多一個符號造成
    userAnswer[count].equals(correctIndividualAnswer[innerCount] 用ASCII碼比較的時候多一個
    因此，我改成這樣 stringBuilderForAnswer.append(data); 由於存答案的時候我都只有1行(題目才需要換行繼續結合)
    解決了這個問題

--- ReadFileByRequire.java
+++ ReadFileByRequire.java
@@ -23,17 +23,27 @@ public class ReadFileByRequire{
 			String answer = test1.getAnswer(chooseQuestion);
 			String[] alreadyAnswer = test1.handleAnswer(answer);
 
-			for(int i = 0; i < alreadyAnswer.length; i++){
-				System.out.println(alreadyAnswer[i]);
-			}
+			Scanner input1 = new Scanner(System.in);
+			String userAnswer = input1.nextLine();
+			handleQuestion test2 = new handleQuestion();
+			String[] alreadyUserAnswer = test2.handleAnswer(userAnswer);
+			String result = test2.correspondAnswer(alreadyAnswer, alreadyUserAnswer);
+			System.out.print(result);
 		} else {
 			System.out.println("Sorry, we can't find the file that decide how many question.");
-		}
-		
-			
+		}	
 	}
 }
 
+/**
+ *  The class handleQuestion will take responsible for Question and answer.
+ *		Have the following method.
+ *			1.	giveQuestion	Read the file question.txt, and return a question which program requires.
+ *			2.	getAnswer		Read the file answer.txt,and return a answer which program requires.
+ *			3.	handleAnswer    For the answer split, and return a string array.
+ *			4.	correspondAnswer	The method will gain two array(One is answer, another is user answer),
+ *									and return a string for the condition of answering question.
+ */
 class handleQuestion{
 	handleQuestion(){
 
@@ -77,8 +87,7 @@ class handleQuestion{
 			catch(IOException e){}
 		} // end finally
 
-		return question;
-				
+		return question;			
 	}// end method giveQuestion
 
 	static String getAnswer(int number){
@@ -105,7 +114,7 @@ class handleQuestion{
 					stringBuilderForAnswer.delete(0, stringForAnswer.length()); // clear the StringBuilder.
 					times = times + 1;
 				} else {	
-					stringBuilderForAnswer.append(data + "\n"); // connect the long answer.
+					stringBuilderForAnswer.append(data); // 2014.9.5 remove \n (For the problem in correspondAnswer method)
 				}
 				
 			}
@@ -122,7 +131,50 @@ class handleQuestion{
 	}// end method getAnswer
 
 	static String[] handleAnswer(String needSeparate){
-		String[] firstHandleAnswer = needSeparate.split(",");
+		String[] firstHandleAnswer = needSeparate.split(","); // First we use , to devide the every blank answer.
 		return firstHandleAnswer;
 	}// end method handleAnswer
+
+	static String correspondAnswer(String[] correctAnswer, String[] userAnswer){
+		/*
+		String[] userAnswerForLack;
+		if(correctAnswer.length != userAnswer.length){
+			userAnswerForLack[correctAnswer.length];
+
+		}Need to exception handle 2014.9.5*/ 
+		int count = 0;
+		int blank = 1;
+		StringBuilder stringBuilderForResult = new StringBuilder();		
+		while(count < correctAnswer.length){
+			String[] correctIndividualAnswer = correctAnswer[count].split("/"); // Because there are several answers to written, we need to split again.
+			/* Program fragment for check the method.
+			System.out.println("count = " + count);
+			System.out.println("blank = " + blank);
+			System.out.println("correctIndividualAnswer length is " + correctIndividualAnswer.length);
+			
+			for(int j = 0; j < correctIndividualAnswer.length; j++) {
+				System.out.println("correctIndividualAnswer index " + j + "is " + correctIndividualAnswer[j]);
+			}
+			
+			System.out.println("userAnswer[count] is " + userAnswer[count]);*/
+			int innerCount = 0;
+			boolean isCorrect = false;
+			while(innerCount < correctIndividualAnswer.length && isCorrect == false){
+				if(userAnswer[count].equals(correctIndividualAnswer[innerCount])) {
+					isCorrect = true;
+					stringBuilderForResult.append("Your answer " + userAnswer[count] + " in the " + blank + " is correct." + "\n");
+				}
+				innerCount = innerCount + 1;
+			}
+
+			if(isCorrect == false){
+				stringBuilderForResult.append("Your answer " + userAnswer[count] + " in the " + blank + " is wrong." + "\n");
+			}
+			count = count + 1; // Control the answer array.
+			blank = blank + 1; // Variable for which one blank.
+		}
+
+		String stringForResult = new String(stringBuilderForResult); // Convert to string.
+		return stringForResult;
+	}// end method correspondAnswer
 }// end class handleQuestion
